0c90aace78af312c9794c2549206a70b30b16bdf
OmarAzizi <oalazizi75@gmail.com>
2024-02-18 15:46:05 +0300

    Fixed the framerate to 60 FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include "../headers/Game.h"
+#include <SDL2/SDL_stdinc.h>
 
 Game* g_game = 0;
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main() {
+    Uint32 frameStart, frameTime;
+
     std::cout << "game init attempt ...\n";
     if (TheGame::Instance()->init("SDL Game", 100, 100, 640, 480, false)) {
         std::cout << "game init success!\n";
         while (TheGame::Instance()->isRunning()) {
+            frameStart = SDL_GetTicks();
+
             TheGame::Instance()->handleEvents();
             TheGame::Instance()->update();
             TheGame::Instance()->render();
 
-            SDL_Delay(10);
+            frameTime = SDL_GetTicks() - frameStart;
+
+            if (frameTime < DELAY_TIME)
+                SDL_Delay((int)(DELAY_TIME - frameTime));
         }
     } else {
         std::cout << "game init faliure - " << SDL_GetError() << "\n";
